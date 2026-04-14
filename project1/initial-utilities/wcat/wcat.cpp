3db326a6618ec0391b907f366665651394188bee
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2026-04-13 18:08:17 -0700

    Update wcat.cpp

--- project1/initial-utilities/wcat/wcat.cpp
+++ project1/initial-utilities/wcat/wcat.cpp
@@ -1,53 +1,67 @@
-//Wwcat -- Aleena Basil
+//Wzip -- Aleena
 #include<iostream>
 #include<fcntl.h>
 #include<unistd.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-    //ssize_t read(size_t count; int fd, void buf[count], size_t count);
-    //linux manual
+int main(int argc, char *argv[]) {
     const int buff_num = 4096;
     char buff[buff_num];
+    int file_desc;
+    char curr = '\0';
+    int count = 0;
 
-
-    //check if there are files
-    if (argc < 2){
-        return 0;
+    ///check if files were provided
+    if(argc < 2){
+        cout << "wzip: file1 [file2 ...]" << endl;
+        return 1;
     }
 
-    //loop through each file
-    for(int i = 1; i < argc; i++){
-        //read mode
-        int file_desc = open(argv[i], O_RDONLY);
-        //check if it failed to open
+    //go through each file
+    for(int i = 1; i <argc; i++){
+        file_desc = open(argv[i], O_RDONLY);
+        //if file cannot be opened
         if(file_desc < 0){
-            cout << "wcat: cannot open file" << endl;
+            cout << "wzip: cannot open file" << endl;
             return 1;
         }
-        
-        //reaf each file in chunks until reach the end of the file
-        ssize_t readF;
-         while ((readF = read(file_desc, buff, buff_num)) > 0) {
-            //standard output
-            ssize_t writeF = write(STDOUT_FILENO, buff, readF);
-            // check if writing failed
-            if (writeF < 0) {
-                close(file_desc);
-                return 1;
+
+        ssize_t res;
+
+        //read file into buffer until the end of the file
+        while((res = read(file_desc, buff, buff_num)) > 0){
+            //go through each charracter
+            for(int j = 0; j < res; j++){
+                //same character, increase count
+                if(curr == buff[j]){
+                    count++;
+                }
+                else{
+                    //write prev
+                    if(count > 0){
+                        write(STDOUT_FILENO, &count, sizeof(count));
+                        write(STDOUT_FILENO, &curr, sizeof(curr));
+                    }
+                    curr = buff[j];
+                    count = 1;
+                }
             }
         }
-        //check if read failed
-        if(readF < 0){
-            close(file_desc);
-            return 1; 
-        }
-        //file close
-        if(close(file_desc) < 0){
-            return 1; 
+        //read error
+        if(res < 0){
+            return 1;
         }
 
+        if(close(file_desc) < 0){
+            return 1;
+        }
+
+    }
+    //last char count
+    if(count > 0){
+        write(STDOUT_FILENO, &count, sizeof(count));
+        write(STDOUT_FILENO, &curr, sizeof(curr));
     }
     return 0;
 }
